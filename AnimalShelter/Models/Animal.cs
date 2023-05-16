d15f55f47e093b81f487c1529e954412298333b1
Emma Gerigscott <elgerig@gmail.com>
2023-05-16 14:13:16 -0700

    fix enums type

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,15 +1,28 @@
 
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace AnimalShelter.Models
 {
     public class Animal
     {
-        public string Type { get; set; }
+        public AnimalType Type { get; set; }
         public string Name { get; set; }
         public string Breed { get; set; }
         public DateTime Date { get; set; }
         public int AnimalId { get; set; }
+    }
+
+    public enum AnimalType
+    {
+        Dog,
+        Cat,
+        Bird,
+        Fish,
+        Horse,
+        Dragon,
+        Squacker,
+        Duck
     }
 }
